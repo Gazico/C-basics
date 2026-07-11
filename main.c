b7d82bb45bb32ba693e61ba44700ed997e05d40c
Gazico <gazitahir50@gmail.com>
2026-07-11 10:57:32 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-    int a, b H;
+
+int a, b, H;
   printf("Enter two numbers:");
   scanf("%d %d", &a, &b);
 
-  for(a<b?a:b: H>=1; YH--){
+  for(a<b?a:b; H>=1; H--)
+  {
+      if(a%H==0 && b%H==0)
      break;
   }
   printf("Hcf of given numbers is %d", H);
   return 0;
  }
 
-}
