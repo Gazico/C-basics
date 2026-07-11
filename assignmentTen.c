88732e2348d9acf011dc4f381f7b482b21c95e9b
Gazico <gazitahir50@gmail.com>
2026-07-11 10:55:34 +0530

    Add files via upload

---
+++ assignmentTen.c
@@ -0,0 +1,90 @@
+#include<stdio.h>
+int main()
+{  // check whether given number is there in fibo series.
+
+  /*
+   int x, a= -1, b= 1, c,i;
+   printf("Enter a number");
+   scanf("%d", &x);
+
+   for(i=0;   ;i++){
+    c=a+b;
+   }
+   if(c==x){
+
+       printf("%d is the %d term  of the series", x, c);
+       break;
+       }
+
+   if(c>x){
+
+
+       printf("%d is not the %d term of series of ",x);
+       break;
+       }
+   a=b;
+   b=c;
+   return 0;
+
+
+}
+ */
+
+
+  /*
+  int a, b, H;
+  printf("Enter two numbers:");
+  scanf("%d %d", &a, &b);
+
+  for(H=a<b?a:b; H>=1; H--){
+        if(a%H==0 && b%H==0)
+     break;
+  }
+  printf("Hcf of given numbers is %d", H);
+  return 0;
+ }
+*/
+
+/*
+    int a, b, H;
+    printf("Enter two numbers:");
+    scanf("%d %d", &a, &b);
+
+  for(a<b?a:b; H>=1; H--){
+        if(a%H==0 && b%H==0)
+
+     break;
+  }
+  printf("Hcf of given numbers is %d:/n", H);
+
+    if(H==1){
+    printf("co-prime");
+
+  }
+  return 0;
+}
+
+*/
+
+    /*
+   int i, x, u, l;
+   u=100;
+   l=2;
+   for(x=l; x<=u; x++)
+   {
+       for(i=2; i<x; i++)
+        if(x%i==0)
+        break;
+       if(i==x)
+        printf(" %d ", x );
+
+     }
+       return 0;
+
+   }
+
+   */
+
+
+
+
